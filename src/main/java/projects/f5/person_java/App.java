92798b993911bc240427eb5277644cbf1af34051
tizzifona <tizzifona@gmail.com>
2024-10-27 16:02:47 +0100

    Added second person in App.java

--- src/main/java/projects/f5/person_java/App.java
+++ src/main/java/projects/f5/person_java/App.java
@@ -2,9 +2,12 @@ package projects.f5.person_java;
 
 public class App {
     public static void main(String[] args) {
-        Person person = new Person("Nadiia", "Alaieva", "FD12334", 1989, "Ukraine", 'F');
+        Person person1 = new Person("Nadiia", "Alaieva", "FD12334", 1989, "Ukraine", 'F');
+        Person person2 = new Person("Jonny", "Depp", "YR123565", 1963, "USA", 'M');
+        person1.printDetails();
+        System.out.println();
+        person2.printDetails();
 
-        person.printDetails();
     }
 
 }
